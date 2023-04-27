a807ed9b32f61509c47aefa1f7b905ff743546f7
Aakriti <aakritijain2401@gmail.com>
2023-04-27 22:57:33 +0530

    update

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export const Config = {
     prod: {
-        API_URL: "https://gateway.marvel.com/v1",
-        API_KEY_PUBLIC: "0bed0540b6a4641e5114f6244d313456",
-        API_KEY_PRIVATE: "69ab112ae91c1dd64aad2508c14bc62fd28e88ff"
+        API_URL: process.env.API_URL,
+        API_KEY_PUBLIC: process.env.API_KEY_PUBLIC,
+        API_KEY_PRIVATE: process.env.API_KEY_PRIVATE
     }
 }
