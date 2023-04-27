c2dd10387dc75a0bb3b5a7a13f918a9d17172af7
Aakriti <aakritijain2401@gmail.com>
2023-04-27 22:58:06 +0530

    Updates

--- src/utils/config.js
+++
@@ -1,7 +0,0 @@
-export const Config = {
-    prod: {
-        API_URL: process.env.API_URL,
-        API_KEY_PUBLIC: process.env.API_KEY_PUBLIC,
-        API_KEY_PRIVATE: process.env.API_KEY_PRIVATE
-    }
-}
